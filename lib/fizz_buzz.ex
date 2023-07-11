eff2cac860422c125955ed42f4914c103c98b67a
Pietro Martins de Oliveira <desktop-qn01lkbpietro@DESKTOP-QN01LKB.localdomain>
2023-07-11 08:58:39 -0300

    Finished fizzbuzz and tests

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -12,8 +12,37 @@ defmodule FizzBuzz do
     |> handle_file_read()
   end
 
-  def handle_file_read({:ok, result}), do: result
+  # Aula 16, 6:28
+  # def handle_file_read({:ok, result}) do
+  #   #extrai os elementos da string lida do arquivo, separando por vírgula
+  #   list = String.split(result, ",")
+  #   #Enum.map(list, fn number -> String.to_integer(number) end) # forma convencional de utilizar o Enum.map: fun. anôn.
 
-  def handle_file_read({:error, reason}), do: reason
+  #   #converte cada elemento da lista de strings em um integer
+  #   Enum.map(list, &String.to_integer/1) # Sintaxe otimizada para quando tenho apenas 1 parâmetro na fun. passada ao Enum.map
+  # end
 
+  defp handle_file_read({:ok, result}) do
+    result =
+      result
+      |> String.split(",")
+      |> Enum.map(&convert_and_evaluate_numbers/1)
+
+    {:ok, result}
+  end
+
+  defp handle_file_read({:error, reason}), do: {:error, "Error reading the file: #{reason}"}
+
+  defp convert_and_evaluate_numbers(elem) do
+    elem
+    |> String.to_integer()
+    |> evaluate_numbers()
+  end
+
+  # Guards (filtros para os argumentos)
+
+  defp evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  defp evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_numbers(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate_numbers(number), do: number
 end
--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -1,8 +1,17 @@
 defmodule FizzBuzzTest do
   use ExUnit.Case
-  doctest FizzBuzz
 
-  test "greets the world" do
-    assert FizzBuzz.hello() == :world
+  describe "build/1" do
+    test "when a valid file is provided, returns converted list" do
+      expected_response = {:ok, [1, 2, :fizz, 4, :buzz, :buzz, :fizzbuzz, :buzz]}
+
+      assert FizzBuzz.build("numbers.txt") == expected_response
+    end
+
+    test "when an invalid file is provided, returns error" do
+      expected_response = {:error, "Error reading the file: enoent"}
+
+      assert FizzBuzz.build("xerxes.txt") == expected_response
+    end
   end
 end
